a48216cac89bb54d1f10fec2cfc8bf3a27e7288c
Barrek Roberts <b-roberts@users.noreply.github.com>
2018-03-12 15:05:14 -0500

    Complete File Import

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\DataFormats\Ofx;
-use App\DataFormats\Qbo;
 use App\Transaction;
 use App\Account;
 
@@ -30,44 +28,40 @@ class ImportController extends Controller
           'account_id' => 'required|exists:accounts,id',
           'statement' => 'required|mimetypes:text/plain',
         ]);
-
         $statementFile = $request->file('statement');
-
-        //Determine the DataFormat Processor
-        $fileType = studly_case($statementFile->getClientOriginalExtension());
-        switch (strtolower()) {
-           case 'ofx':
-            $parser = new Ofx();
-            break;
-           case 'qbo':
-            $parser = new Qbo();
-            break;
-           default:
-            session()->flash('flash_error', 'Cannot parse this file type.');
-
-            return back();
-         }
-
-        $path = storage_path('app/'.$statementFile->store('statements'));
-
+        $accountID = $request->input('account_id');
+        $ofxParser = new \OfxParser\Parser();
+        $ofx = $ofxParser->loadFromFile($statementFile);
+        $bankAccount = reset($ofx->bankAccounts);
         /**
          * @var string Used to group transactions by import batch for easier duplication resolution
          */
         $timestamp = date('Y-m-d H:i:s');
 
-        $transactions = $parser->parse($path);
+        // Get the statement transactions for the account
+        $transactions = $bankAccount->statement->transactions;
 
         $importCount = 0;
-        foreach ($transactions as $transaction) {
-            if (0 == Transaction::where('fitid', $transaction->fitid)->count()) {
-                $transaction->account_id = $request->input('account_id');
+        foreach ($transactions as $ofxTransaction) {
+            if (0 == Transaction::where('fitid', $ofxTransaction->uniqueId)->count()) {
+                $transaction = new Transaction();
+                $transaction->account_id = $accountID;
                 $transaction->created_at = $timestamp;
+                $transaction->value = -1 * $ofxTransaction->amount;
+                $transaction->location = $ofxTransaction->name;
+                $transaction->fitid = $ofxTransaction->uniqueId;
+                $transaction->date = $ofxTransaction->date->format('Y-m-d');
                 if ($transaction->save()) {
                     ++$importCount;
                 }
             }
         }
-        $existingCount = $transactions->count() - $importCount;
+
+        if (0 == \DB::table('account_balance')->where(['account_id' => $accountID, 'date' => $bankAccount->balanceDate->format('Y-m-d')])->count()) {
+            \DB::table('account_balance')->insert(['account_id' => $accountID, 'date' => $bankAccount->balanceDate->format('Y-m-d'), 'value' => $bankAccount->balance * -1]);
+        }
+
+        $existingCount = sizeof($transactions) - $importCount;
         session()->flash('flash_success', "$importCount transactions imported. $existingCount transactions already exist.");
 
         return back();
--- resources/views/pages/transactions/index.blade.php
+++ resources/views/pages/transactions/index.blade.php
@@ -1,4 +1,7 @@
 @extends('templates.bootstrap')
 @section('content')
+  <div>
+    <a href="{{route('import.index')}}" class="pull-right">Import from financial institution</a>
+  </div>
 @include('modules.transactions_table')
 @endsection
